eb3b7609788fbe79b330fc78e5fccda69db8190d
PaulinaCoding <paulina.pietrykiewicz@gmail.com>
2018-05-11 02:34:01 +0000

    Added extra comments and solved one bug.

--- sort-array2.js
+++ sort-array2.js
@@ -1,12 +1,18 @@
+/*Write a sorting function (sometimes called a custom comparator), that sorts the above array first by the name ascending alphabetically, and in cases where the names are equal sort by descending age. */
+
 var students = [
   { id: 1, name: "bruce",    age: 40 },
   { id: 2, name: "zoidberg", age: 22 },
   { id: 3, name: "alex",     age: 22 },
-  { id: 4, name: "alex",     age: 30 }
-];
+  { id: 4, name: "alex",     age: 30 },
+// Some extra objects to test it for the correct input.
+  //{ id: 4, name: "alex",     age: 17 },
+//{ id: 4, name: "alex",     age: 19 }
 
-//this part is sorting students  by name
-//this is an eample of anonymous callback function
+];
+//this part is sorting students by name
+//this is an exmple of anonymous callback function
+
 students.sort(function(a, b) {
   var nameA = a.name.toUpperCase(); // ignore upper and lowercase
   var nameB = b.name.toUpperCase(); // ignore upper and lowercase
@@ -17,11 +23,12 @@ students.sort(function(a, b) {
     return 1;
      }
      if (nameA = nameB) { 
-  //here because we have 2 students with the same name we sort by age
-        return students.sort(function (a, b) {
+//here because we have 2 students with the same name we sort by age
+// if you use this function in note below it will double the "bruce name
+        //return students.sort(function (a, b) {
         return a.age - b.age;
-        });
+        };
     
     }
-});
+);
 console.log(students);
